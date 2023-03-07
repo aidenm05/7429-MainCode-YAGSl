37193a0a87582fd9245198cd6e5b7d9ad9d4baba
aiden mcmanus <Aidenpmcmanus@gmail.com>
2023-03-06 21:46:45 -0500

    yeah

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,12 +27,14 @@ public final class Constants
   public static final class Auton
   {
 
-    public static final PIDFConfig xAutoPID     = new PIDFConfig(0.7, 0, 0);
+    public static final PIDFConfig xAutoPID     = new PIDFConfig(0.7, 0, 0); // x is not used y is used for both universially
     public static final PIDFConfig yAutoPID     = new PIDFConfig(0.7, 0, 0);
-    public static final PIDFConfig angleAutoPID = new PIDFConfig(0.4, 0, 0.01);
+    public static final PIDFConfig angleAutoPID = new PIDFConfig(0.4, 0, 0.01);// <----- used
 
-    public static final double MAX_SPEED        = 4;
-    public static final double MAX_ACCELERATION = 2;
+    // These are not used
+  //  public static final double MAX_SPEED        = 4;
+  //  public static final double MAX_ACCELERATION = 2;
+    // These are not used
   }
 
   public static final class Drivebase
